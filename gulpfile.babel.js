8682c14d73a2a046e7d7b812f3ae2c02e10917cb
Михаил <p.mihail174@yandex.ru>
2024-05-28 17:49:06 +0500

    add build files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,6 +10,6 @@ export const development = gulp.series("clean-dev",
     gulp.parallel(["pug", "fonts", "sprite", "image", "style", "js"]),
     gulp.parallel("server"));
 
-export const prod = gulp.series("clean-build", gulp.series(['pug', "fonts", "sprite", "image", "style", "js", "zip"]));
+export const prod = gulp.series("clean-build", gulp.series(['pug', "fonts", "sprite", "image", "style", "js", "replace-url", "zip"]));
 
 export default development;
--- tasks/replace-url.js
+++ tasks/replace-url.js
@@ -5,7 +5,8 @@ import replace from "gulp-replace";
 
 gulp.task("replace-url", () => {
     return gulp.src('build/**/*')
-        .pipe(replace('href="../../assets/', 'href="'))
-        .pipe(replace('src="../../assets/', 'src="'))
+        // .pipe(replace('href="../../assets/', 'href="'))
+        .pipe(replace('src="/assets/', 'src="assets/'))
+        // .pipe(replace('src="../../assets/', 'src="'))
         .pipe(gulp.dest('build'))
 });
